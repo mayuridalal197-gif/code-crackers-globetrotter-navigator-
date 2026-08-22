414852aa227c67e18f545d276331f4b6e462bd80
mayuridalal197-gif <mayuridalal197@gmail.com>
2026-08-22 14:56:24 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,20 @@
 const express = require("express");
+const cors = require("cors");
 require("dotenv").config();
 
 const { testDatabaseConnection } = require("./_config/database");
+
 const authRoutes = require("./apiRoutes/authRoutes");
 const tripRoutes = require("./apiRoutes/tripRoutes");
 const itineraryRoutes = require("./apiRoutes/itineraryRoutes");
 const budgetRoutes = require("./apiRoutes/budgetRoutes");
+const destinationRoutes = require("./apiRoutes/destinationRoutes");
 
 const app = express();
+
+app.use(cors({
+    origin: "http://127.0.0.1:5500"
+}));
 
 app.use(express.json());
 
@@ -15,6 +22,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/trips", tripRoutes);
 app.use("/api/itinerary", itineraryRoutes);
 app.use("/api/budget", budgetRoutes);
+app.use("/api/destinations", destinationRoutes);
 
 const PORT = process.env.PORT || 5000;
 
