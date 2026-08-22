4945a216cbc1525fc0f33879e6a44eee05f03ebc
mayuridalal197-gif <mayuridalal197@gmail.com>
2026-08-22 11:45:02 +0530

    Update server.js
    start point to enter in backend

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,20 @@
+const express = require("express");
+require("dotenv").config();
 
+const { testDatabaseConnection } = require("./_config/database");
+const authRoutes = require("./apiRoutes/authRoutes");
+
+const app = express();
+
+app.use(express.json());
+
+// Authentication routes
+app.use("/api/auth", authRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+testDatabaseConnection();
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
